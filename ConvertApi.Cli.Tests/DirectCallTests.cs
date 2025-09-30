d5390596712a80e1eb4a27f4ea3e0b8c65bded92
Tomas Rutkauskas <tomas@baltsoft.com>
2025-09-30 14:40:18 +0300

    Add new tests for custom file naming and safe file generation
    Introduced unit tests to validate output file naming and safe file generation scenarios. Ensured existing tests maintain consistent formatting and alignment adjustments. Improved test reliability by verifying file creation outputs.

--- ConvertApi.Cli.Tests/DirectCallTests.cs
+++ ConvertApi.Cli.Tests/DirectCallTests.cs
@@ -18,6 +18,30 @@ public class DirectCallTests
     }
 
     [Test]
+    public async Task TestSetOutputFileName()
+    {
+        var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
+        var copyOfInputFile = Path.Combine(TestOutputDir, "simple.pdf");
+        File.Copy(inputFile, copyOfInputFile);
+        var outputFile = Path.Combine(TestOutputDir, "simple2.pdf");
+
+        await Program.Main([ApiToken, outputFile, copyOfInputFile, "pdf", "repair"]);
+
+        Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
+    }
+
+    [Test]
+    public async Task TestGenerateSafeFiles()
+    {
+        var outputFile = Path.Combine(TestOutputDir, "output.png");
+        var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "22pages.pdf");
+
+        await Program.Main([ApiToken, outputFile, inputFile]);
+
+        Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
+    }
+    
+    [Test]
     public async Task TestConvertPdfToDocx()
     {
         var outputFile = Path.Combine(TestOutputDir, "simple2.docx");
@@ -34,12 +58,12 @@ public class DirectCallTests
         var outputFile = Path.Combine(TestOutputDir, "simple.pdf");
         var inputFile1 = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
         var inputFile2 = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "invoice.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile1, inputFile2, "pdf", "merge"]);
-    
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestMergeMutiplePdfs()
     {
@@ -47,77 +71,77 @@ public class DirectCallTests
         var inputFile1 = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
         var inputFile2 = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "invoice.pdf");
         var inputFile3 = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "invoice.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile1, inputFile2, inputFile3, "pdf", "merge"]);
-    
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestAddWatermarkToPdf()
     {
         var outputFile = Path.Combine(TestOutputDir, "watermark.pdf");
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "pdf", "watermark", "Text=Confidential", "FileName=watermark"]);
-    
-    
+
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestAddWatermarkOverlayToPdf()
     {
         var outputFile = Path.Combine(TestOutputDir, "watermarkOverlay.pdf");
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
         var overlayFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "pdf", "watermark-overlay", $"OverlayFile={overlayFile}", "FileName=watermarkOverlay"]);
-    
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestDocxCompare()
     {
         var outputFile = Path.Combine(TestOutputDir, "demo.docx");
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "demo.docx");
         var compareFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "demo-changed.docx");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "docx", "compare", $"CompareFile={compareFile}", "storeFile=false"]);
-    
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestProtectPdfWithPassword()
     {
         var outputFile = Path.Combine(TestOutputDir, "protected.pdf");
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "pdf", "protect", "UserPassword=1234", "OwnerPassword=abcd", "FileName=protected"]);
-    
+
         Assert.IsTrue(File.Exists(outputFile), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestPdfToPngWithResolution()
     {
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "22pages.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "pdf", "png", "Resolution=300"]);
 
         var outputFiles = Directory.GetFiles(TestOutputDir);
         Assert.That(outputFiles, Has.Length.EqualTo(22), "Output files were not created.");
     }
-    
+
     [Test]
     public async Task TestPdfToExtractImages()
     {
         var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "../../../../", "test_files", "simple.pdf");
-    
+
         await Program.Main([ApiToken, TestOutputDir, inputFile, "pdf", "extract-images"]);
-    
+
         var outputFiles = Directory.GetFiles(TestOutputDir);
         Assert.That(outputFiles, Has.Length.EqualTo(1), "Output file was not created.");
     }
@@ -126,16 +150,16 @@ public class DirectCallTests
     public async Task TestWebToPdf()
     {
         await Program.Main([ApiToken, TestOutputDir, "", "web", "pdf", "url=https://www.google.com"]);
-        
+
         var outputFiles = Directory.GetFiles(TestOutputDir);
         Assert.That(outputFiles, Has.Length.EqualTo(1), "Output file was not created.");
     }
-    
+
     [Test]
     public async Task TestWebToPdfWithNullasInput()
     {
         await Program.Main([ApiToken, TestOutputDir, null, "web", "pdf", "url=https://www.google.com"]);
-        
+
         var outputFiles = Directory.GetFiles(TestOutputDir);
         Assert.That(outputFiles, Has.Length.EqualTo(1), "Output file was not created.");
     }
